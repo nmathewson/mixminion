ce4d230fe1329c71f8cc8181413824f5fce9293d
Nick Mathewson <nickm@freehaven.net>
2003-06-26 03:19:29 +0000

    Fix bug noticed by Marco -- tm versus time.time()

--- lib/mixminion/test.py
+++ lib/mixminion/test.py
@@ -1,5 +1,5 @@
 # Copyright 2002-2003 Nick Mathewson.  See LICENSE for licensing information.
-# $Id: test.py,v 1.123 2003/06/21 21:47:50 nickm Exp $
+# $Id: test.py,v 1.124 2003/06/26 03:19:29 nickm Exp $
 
 """mixminion.tests
 
@@ -5057,12 +5057,11 @@ class ServerMainTests(unittest.TestCase):
         self.assertEquals(s.firstEventTime(), tm+1.5)
         s.processEvents(tm+1.5)
         self.assertEquals(["c"], lst)
-        diff = abs(s.firstEventTime()-(time.time()+1))
+        diff = abs(s.firstEventTime()-(tm+1))
         self.assert_(diff < 0.01)
 
         s.processEvents(tm+5)
         self.assertEquals(["c", "c", "d", "b"], lst)
-
 
     def testMixPool(self):
         ServerConfig = mixminion.server.ServerConfig.ServerConfig
